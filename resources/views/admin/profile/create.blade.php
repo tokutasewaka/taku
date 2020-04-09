ad873e8c61bdb057428798102a7341002e42f4e7
kawase takuto <tokutasewaka307@gmail.com>
2020-04-09 05:13:37 +0000

    php

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,12 +1,13 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible"content="IE=edge">
-        <meta name="viewport"content="width=device,initial=scale=1">
-        <title>Mynews</title>
-    </head>
-    <body>
-        <h1>Myニュース作成画面</h1>
-    </body>
-</html>
+@extends('layouts.profile')
+
+@section('title','My プロフィールの新規作成')
+
+@section('content')
+  <div class="container">
+      <div class="row">
+          <div class="col-md-8 mx-auto">
+             <h2>My プロフィール </h2>
+          </div>
+      </div>
+  </div>
+@endsection
